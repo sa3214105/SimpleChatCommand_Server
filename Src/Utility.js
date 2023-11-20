914e13086fc5db3843f0e8fc89500cc6727ee0b8
sa3214105 <50324550+sa3214105@users.noreply.github.com>
2023-11-21 00:47:20 +0800

    chore:Remove unreferenced files

--- Src/Utility.js
+++
@@ -1,6 +0,0 @@
-export function InstanceOf_Soft(_class,_obj){
-   return IsEqual(Object.assign(new _class(),_obj),_obj);
-}
-export function IsEqual(obj1,obj2){
-    return JSON.stringify(obj1)===JSON.stringify(obj2);
-}
